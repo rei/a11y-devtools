595f0b1216b0355045dc473fecdab844bfbbc972
Alex Perkins <aperkin@rei.com>
2017-09-05 10:52:12 -0700

    Update example

--- example/client.js
+++ example/client.js
@@ -14,7 +14,8 @@ let ra = mod.init({
 })
 
 // Test 1
-// 1st arg: the tenon docID (Note, it must be
+// 1st arg: the tenon docID (Note, it must be >= 16 characters)
+// 2nd arg: the puppeteer navigation commands controlling Chrome.
 ra.run('rei.com_learn_page', async page => {
   await page.goto('http://www.rei.com/learn.html')
 });
